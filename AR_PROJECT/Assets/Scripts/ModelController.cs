2ddc70599c7bc1d373d27b9e7d8ca5cdf0e7b78f
Moromon <lmoralvxd@gmail.com>
2023-05-02 19:24:24 +0200

    q

--- AR_PROJECT/Assets/Scripts/ModelController.cs
+++ AR_PROJECT/Assets/Scripts/ModelController.cs
@@ -1,51 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ModelController : MonoBehaviour
 {
-
     private float initialDistance;
     private Vector3 initialScale;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.touchCount == 2)
+        if (Input.touchCount == 2)
         {
             var touchZero = Input.GetTouch(0);
             var touchOne = Input.GetTouch(1);
-            
-            if(touchZero.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled || touchOne.phase == TouchPhase.Canceled)
+
+            if (touchZero.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled || touchOne.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Canceled)
             {
                 return;
             }
 
-            if(touchZero.phase == TouchPhase.Began || touchOne.phase == TouchPhase.Began)
+            if (touchZero.phase == TouchPhase.Began || touchOne.phase == TouchPhase.Began)
             {
                 initialDistance = Vector2.Distance(touchZero.position, touchOne.position);
                 initialScale = transform.localScale;
             }
             else
             {
-                float currentDistance = Vector2.Distance(touchZero.position,touchOne.position);
+                float currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
 
-                if (Mathf.Approximately(initialDistance, 0))
+                if (Mathf.Approximately(initialDistance, 0)|| Mathf.Approximately(initialDistance,currentDistance))
                 {
                     return;
                 }
 
                 float factor = currentDistance / initialDistance;
-                transform.localScale =initialScale*factor;
-
+                transform.localScale = initialScale * factor;
             }
-
         }
     }
 }
