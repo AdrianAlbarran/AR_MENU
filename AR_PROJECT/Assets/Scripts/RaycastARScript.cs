fd929b2fa1dbaf0f2d39b22245090c7707c1bc15
AdrianAlbarran <adrian-240102@hotmail.es>
2023-06-02 12:59:48 +0200

    detectamos colision con objetos

--- AR_PROJECT/Assets/Scripts/RaycastARScript.cs
+++ AR_PROJECT/Assets/Scripts/RaycastARScript.cs
@@ -10,14 +10,20 @@ public class RaycastARScript : MonoBehaviour
     public GameObject spawn_prefab;
     private GameObject _spawned_object;
     private bool _ObjectSpawned;
+    public ARPlaneManager planeManager;
 
-    ARRaycastManager arrayMan;
+    [SerializeField]
+    public Camera arCamera;
+
+    private ARRaycastManager raycastManager;
     List<ARRaycastHit> hits = new List<ARRaycastHit>();
 
     private void Start()
     {
         _ObjectSpawned = false;
-        arrayMan = GetComponent<ARRaycastManager>();
+        raycastManager = GetComponent<ARRaycastManager>();
+        planeManager = GetComponent<ARPlaneManager>();
+
 
     }
 
@@ -25,18 +31,41 @@ public class RaycastARScript : MonoBehaviour
     {
         if (Input.touchCount == 1)
         {
-            if (arrayMan.Raycast(Input.GetTouch(0).position, hits, TrackableType.PlaneWithinPolygon))
+            Touch touch = Input.GetTouch(0);
+            if (raycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
             {
                 var hitPose = hits[0].pose;
                 if (!_ObjectSpawned)
                 {
                     Vector3 newPos = new Vector3(hitPose.position.x, hitPose.position.y + 0.05f, hitPose.position.z); 
                     _spawned_object = Instantiate(spawn_prefab, hitPose.position, hitPose.rotation);
+                    _spawned_object.name = "campana";
+                    _spawned_object.transform.localScale = new Vector3(0.12f, 0.12f, 0.12f);
                     _ObjectSpawned = true;
+
+                    foreach (var plane in planeManager.trackables)
+                    {
+                        plane.gameObject.SetActive(false);
+
+                        planeManager.enabled = false;
+
+                    }
                 }
                 else
                 {
-                    _spawned_object.transform.position = hitPose.position;
+                    Ray ray = arCamera.ScreenPointToRay(touch.position);
+                    RaycastHit hitObject;
+                    Debug.Log("aaaaaaaaaaaaaaaaaaaaaaaaaa");
+
+                    bool test = Physics.Raycast(ray, out hitObject);
+                    if (test)
+                    {
+                        Debug.Log("Colisiono con: " + hitObject.transform.name);
+                    }
+                    else
+                    {
+                        Debug.Log(test);
+                    }
                 }
             }
         }
