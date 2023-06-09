53285d19d61d6b3b6aff81f5cfe1824a11d1f50e
AdrianAlbarran <adrian-240102@hotmail.es>
2023-06-09 15:15:37 +0200

    se mueve :D

--- AR_PROJECT/Assets/Scripts/Game/PlayerController.cs
+++ AR_PROJECT/Assets/Scripts/Game/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour, ISubject<int>
     private bool isMoving = false;
     private Vector3 vecToCamera;
     private Rigidbody _rb;
+    private float dis;
 
     public int score;
     void Start()
@@ -25,35 +26,41 @@ public class PlayerController : MonoBehaviour, ISubject<int>
     // Update is called once per frame
     void Update()
     {
-        pos = transform.position;
-        Vector3 look = cameraSceneAR.transform.forward;
-        look.Normalize();
-        vecToCamera = this.pos - cameraSceneAR.transform.position;
-        vecToCamera.Normalize();
-        Debug.Log(vecToCamera);
-
-        // angle in [0,180]
-        float angle = Vector3.Angle(look, vecToCamera);
-        float sign = Mathf.Sign(Vector3.Dot(look, Vector3.Cross(new Vector3(0, 1, 0), vecToCamera)));
-
-        // angle in [-179,180]
-        float signed_angle = angle * sign;
-
-        Debug.Log("angulo: " + angle);
-        Debug.Log("angulo con signo: " + signed_angle);
-
-        angle = Mathf.Deg2Rad * signed_angle;
-        float dis = Mathf.Sin(angle) * 0.1f;
-
-        Debug.Log(dis);
-
-        _rb.velocity = new Vector3(-this.transform.forward.x * dis, 0, 0); 
         
     }
 
     private void FixedUpdate()
     {
-        //transform.position = new Vector3(camera.transform.position.x, transform.position.y, transform.position.z);
+        pos = transform.position;
+        Vector3 look = cameraSceneAR.transform.forward;
+        look.Normalize();
+        vecToCamera = this.pos - cameraSceneAR.transform.position;
+        vecToCamera.Normalize();
+
+        // angle in [0,180]
+        float angle = Vector3.Angle(new Vector2(look.x, look.z), new Vector2(vecToCamera.x, vecToCamera.z));
+        float sign = Mathf.Sign(Vector3.Dot(look, Vector3.Cross(new Vector3(0, 1, 0), vecToCamera)));
+
+        // angle in [-179,180]
+        float signed_angle = angle * sign;
+
+
+        if (signed_angle > -5 && signed_angle < 5)
+        {
+            _rb.velocity = Vector3.zero;
+        }
+        else
+        {
+            _rb.AddForce(transform.right * dis, ForceMode.VelocityChange);
+
+            Debug.Log("angulo con signo: " + signed_angle);
+
+            signed_angle = Mathf.Deg2Rad * signed_angle;
+            dis = Mathf.Sin(signed_angle);
+            dis = dis >= 0.35f ? dis * .2f : dis * .1f;
+
+            Debug.Log(transform.right);
+        }
     }
 
     public void UpdateScore(int value)
